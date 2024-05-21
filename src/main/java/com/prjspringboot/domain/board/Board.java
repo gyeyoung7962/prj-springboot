34d72b7cebce250753591626dc9dcb73be3183df
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-21 14:28:40 +0900

    게시물 목록 보기

--- src/main/java/com/prjspringboot/domain/board/Board.java
+++ src/main/java/com/prjspringboot/domain/board/Board.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Board {
 
+    private Integer id;
     private String title;
     private String content;
     private String writer;
--- src/main/java/com/prjspringboot/mapper/board/BoardMapper.java
+++ src/main/java/com/prjspringboot/mapper/board/BoardMapper.java
@@ -3,6 +3,9 @@ package com.prjspringboot.mapper.board;
 import com.prjspringboot.domain.board.Board;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
@@ -13,4 +16,12 @@ public interface BoardMapper {
             values (#{title}, #{content}, #{writer})
             """)
     void add(Board board);
+
+    @Select("""
+            select id, title, writer
+            from board
+            order by id desc
+            """)
+    List<Board> selectAll();
+
 }
