258b23e5cc3957609839ac6977a9dec0e31df816
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-21 10:13:19 +0900

    first commit

---
+++ src/main/java/com/prjspringboot/PrjSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.prjspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prjspringboot/PrjSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prjspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
