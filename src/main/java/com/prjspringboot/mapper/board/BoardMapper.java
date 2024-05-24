df7da3df57eb7c949f5df6583a78d760b6cb374b
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-24 10:44:17 +0900

    글 수정시 writer 컬럼 업데이트 코드 삭제

--- src/main/java/com/prjspringboot/mapper/board/BoardMapper.java
+++ src/main/java/com/prjspringboot/mapper/board/BoardMapper.java
@@ -24,7 +24,7 @@ public interface BoardMapper {
     List<Board> selectAll();
 
     @Select("""
-            select b.title, b.content, b.regDate,m.nick_name as writer, b.member_id
+            select b.title, b.content, b.regDate,m.nick_name as writer, b.member_id, b.id
             from board b join member m
             on b.member_id = m.id
             where b.id = #{id}
