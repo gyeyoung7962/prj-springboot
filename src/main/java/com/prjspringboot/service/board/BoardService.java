0b7e71a91562ee0978dd9d738ea6297bc6fe468b
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-27 20:07:41 +0900

    현재 페이지 기준에서 페이지네이션 출력 계산

--- src/main/java/com/prjspringboot/service/board/BoardService.java
+++ src/main/java/com/prjspringboot/service/board/BoardService.java
@@ -46,9 +46,13 @@ public class BoardService {
 
         Integer offset = (page - 1) * 10;
         Integer lastPageNumber = (countAll - 1) / 10 + 1;
+        Integer leftPageNumber = (page - 1) / 10 * 10 + 1;
+        Integer rightPageNumber = leftPageNumber + 9;
 
         pageInfo.put("currentPageNumber", page);
         pageInfo.put("lastPageNumber", lastPageNumber);
+        pageInfo.put("leftPageNumber", leftPageNumber);
+        pageInfo.put("rightPageNumber", rightPageNumber);
 
         return Map.of("boardList", mapper.selectAllPaging(offset),
                 "pageInfo", pageInfo);
